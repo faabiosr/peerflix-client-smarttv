156aac9ab1e67fcfcb9f8ef8c511e91635cd7679
Fabio Ribeiro <fabiorphp@gmail.com>
2014-11-30 01:07:41 -0200

    Main
    * Removed entire player container in favor of new class

--- app/main.js
+++ app/main.js
@@ -1,19 +1,20 @@
-
-define(['app/player', 'jquery'], function(Player) {
+define(['app/player-container', 'jquery'], function(PlayerContainer) {
     "use strict";
 
     function Main() {
         this.formContent     = $('#client');
         this.inputField      = this.formContent.find('input[type=text]');
         this.sendButton      = this.formContent.find('.button');
-        this.playerContainer = $('#player');
-        this.player          = new Player('video-player');
+        this.playerContainer = new PlayerContainer('#player');
     };
 
     Main.prototype.init = function() {
         this.sendButton.on('click', $.proxy(this.open, this));
-
         this.inputField.on('keyup', $.proxy(this.validate, this));
+
+        $(document).on('playercontainer.close', $.proxy(function() {
+            this.formContent.fadeIn();
+        }, this));
     };
 
     Main.prototype.open = function(){
@@ -21,18 +22,11 @@ define(['app/player', 'jquery'], function(Player) {
             return;
         }
 
-        this.formContent.fadeOut($.proxy(this.closeForm, this));
-    }
-
-    Main.prototype.closeForm = function() {
-        this.playerContainer.fadeIn($.proxy(this.openPlayer, this));
-    }
-
-    Main.prototype.openPlayer = function() {
-        this.player
-            .src(this.inputField.val())
-            .play();
-    }
+        this.formContent.fadeOut($.proxy(function() {
+            this.playerContainer.player.src(this.inputField.val());
+            this.playerContainer.open();
+        }, this));
+    };
 
     Main.prototype.validate = function() {
         if (!this.inputField.val()) {
