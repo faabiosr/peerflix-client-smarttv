9236e2d2d4bed5ca763312bd766c6a761f797153
Fabio Ribeiro <fabiorphp@gmail.com>
2014-11-30 00:00:04 -0200

    Player
    * VideoJS Player wrapper

---
+++ app/player.js
@@ -0,0 +1,49 @@
+
+define(['video-js/video'], function() {
+    "use strict";
+
+    function Player(id){
+        this.options = {
+            children: {
+                controlBar: {
+                    children: {
+                        fullscreenToggle: false,
+                        muteToggle: false,
+                        volumeControl: false
+                    }
+                }
+            }
+        };
+
+        this.player  = videojs(id, this.options);
+    };
+
+    Player.prototype.play = function() {
+        this.player.play();
+    };
+
+    Player.prototype.stop = function() {
+        this.player
+            .pause()
+            .currentTime(0);
+
+        return this;
+    };
+
+    Player.prototype.pause = function() {
+        if (this.player.paused()) {
+            this.player.play();
+            return;
+        }
+
+        this.player.pause();
+        return this;
+    };
+
+    Player.prototype.src = function(source) {
+        this.player.src(source);
+        return this;
+    };
+
+    return Player;
+});
